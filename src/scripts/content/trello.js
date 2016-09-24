585627ca61f6fcd53a516921930100d60739d103
mickeyh <mickeyh>
2016-09-23 21:09:50 -0700

    Extend Trello parametrization to checklist buttons too.

--- src/scripts/content/trello.js
+++ src/scripts/content/trello.js
@@ -3,6 +3,19 @@
 
 'use strict';
 
+function harvestTagsFromDescription() {
+  var descriptionText = $('.js-card-desc').innerText;
+  var descriptionTextLines = descriptionText.split(/\s*\r?\n\s*/);
+  var TAG_PREFIX = 'toggltag: ';
+  var tagLineRegex = new RegExp('^' + TAG_PREFIX);
+  var tagLines = descriptionTextLines.filter((l) => {
+    return tagLineRegex.test(l);
+  });
+  return tagLines.map((l) => {
+    return l.slice(TAG_PREFIX.length).trim();
+  });
+}
+
 togglbutton.render('.window-header:not(.toggl)', {observe: true}, function (elem) {
   var link, container = createTag('div', 'button-link trello-tb-wrapper'),
     //duration,
@@ -13,17 +26,6 @@ togglbutton.render('.window-header:not(.toggl)', {observe: true}, function (elem
     projectElem = $('.board-header > a'),
     descriptionElem = $('.js-move-card');
 
-  var descriptionText = $('.js-card-desc').innerText;
-  var descriptionTextLines = descriptionText.split(/\s*\r?\n\s*/);
-  var TAG_PREFIX = 'toggltag: ';
-  var tagLineRegex = new RegExp('^' + TAG_PREFIX);
-  var tagLines = descriptionTextLines.filter((l) => {
-    return tagLineRegex.test(l);
-  });
-  var tags = tagLines.map((l) => {
-    return l.slice(TAG_PREFIX.length).trim();
-  });
-
   descFunc = function () {
     return titleElem.innerText;
   };
@@ -32,7 +34,7 @@ togglbutton.render('.window-header:not(.toggl)', {observe: true}, function (elem
     className: 'trello',
     description: descFunc,
     projectName: projectElem.innerText,
-    tags: tags,
+    tags: harvestTagsFromDescription(),
     calculateTotal: true,
   });
 
@@ -59,6 +61,7 @@ togglbutton.render('.checklist-item-details:not(.toggl)', {observe: true}, funct
     buttonType: 'minimal',
     projectName: projectElem.innerText,
     description: titleElem.innerText + ' - ' + taskElem.innerText,
+    tags: harvestTagsFromDescription(),
   });
 
   link.classList.add('checklist-item-button');
